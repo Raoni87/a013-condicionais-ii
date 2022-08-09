8b0a2be16fcb8bfc439c248bc1e5da92ceb4907f
Raoni87 <raonioliva@gmail.com>
2022-08-08 21:45:40 -0300

    resolução de exercícios

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,15 @@
-const num = //seu numero aqui
+const num = Number(prompt("Digite um número aqui"));
+
+/*1.*/ if(num %2 === 0) {
+    if(num %3 === 0) 
+    console.log("numero é divisível por 2 ou 3")
+} else {
+    console.log("numero não é divisível por 2 ou 3")
+}
+
+/*2.*/ if(num %2 === 0 || num %3 === 0) {
+    console.log(" numer é divisível por 2 ou 3")
+} else {
+    console.log("numero não é divisível por 2 ou 3")
+}
 
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,7 @@
-//código a ser reescrito
+const nome = prompt("Qual seu nome?");
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
+console.log(nome === "José"? "Oi, José":"Olá," + " " + nome);
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+const idade = prompt("Qual sua idade?")
+
+console.log(idade >=18? "pode tirar carteira de motorista!":"Ainda não pode tirar carteira de motorista");
