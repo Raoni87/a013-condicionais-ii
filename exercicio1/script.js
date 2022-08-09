dd52cb653d3158dabe5af866c1a1283a53f73a72
Raoni87 <raonioliva@gmail.com>
2022-08-09 17:15:53 -0300

    resolucao de exercicios

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,14 +1,21 @@
 const num = Number(prompt("Digite um número aqui"));
 
-/*1.*/ if(num %2 === 0) {
-    if(num %3 === 0) 
-    console.log("numero é divisível por 2 ou 3")
-} else {
-    console.log("numero não é divisível por 2 ou 3")
+if (num % 2 === 0) {
+    if (num % 3 === 0) {
+        console.log(`Esse numero ${num} e divisível por 2 ou por 3 `)
+    } else if (!(num % 3 === 0)) {
+        console.log(`Esse numero ${num} e divisível por 2 ou por 3 `)
+    }
+} else if (!(num % 2 === 0)) {
+    if (num % 3 === 0) {
+        console.log(`Esse numero ${num} e divisível por 2 ou por 3 `)
+    } else {
+        console.log(`Esse numero ${num} não e divisível por 2 ou por 3  `)
+    }
 }
 
 /*2.*/ if(num %2 === 0 || num %3 === 0) {
-    console.log(" numer é divisível por 2 ou 3")
+    console.log("numero é divisível por 2 ou 3")
 } else {
     console.log("numero não é divisível por 2 ou 3")
 }
